c2d655b014962fd03e5f695b9ad93bb1c2cb5232
christina <christina.lopes@epita.fr>
2025-11-20 10:52:35 +0100

    change project

---
+++ ProxiBanque/src/main/java/org/example/proxibanque2/ProxiBanque2Application.java
@@ -0,0 +1,13 @@
+package org.example.proxibanque2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanque2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanque2Application.class, args);
+    }
+
+}
